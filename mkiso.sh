3735f43508d5983483e915b6e4a28f83b1128865
leaeasy <leaeasy@gmail.com>
2018-05-21 14:42:59 +0800

    add graphics detect support

--- mkiso.sh
+++ mkiso.sh
@@ -1,4 +1,6 @@
 LIVEKITDATA=/tmp/iso
+_ISOHDPFX=$(pwd)/isohdpfx.bin
 cd $LIVEKITDATA && xorriso -as mkisofs -o /tmp/iso.iso -v -J -R -D \
+	-isohybrid-mbr ${_ISOHDPFX} -partition_offset 16 \
 	-no-emul-boot -boot-info-table -boot-load-size 4 \
 	-b slax/boot/isolinux.bin -c slax/boot/isolinux.boot . 
